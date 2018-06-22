82a021bd66a437950bd2c15f881837fd86d7cd57
Davidjeje <david.marivat-larose@orange.fr>
2018-06-22 12:13:12 +0200

    ajout du fichier et du code qui permet de supprimer un blog post

---
+++ controleur/sup.php
@@ -0,0 +1,25 @@
+<?php
+function sup()
+{
+	include(dirname(__FILE__).'/../modele/news.php');
+	if (!empty($_POST['idd']) and !empty($_POST['user']) and!empty($_POST['author']) and !empty($_POST['title']) and !empty($_POST['chapo']) and !empty($_POST['content'])  and !empty($_POST['change_date']))
+	{
+		$id = $_POST['idd'];
+		$user_id = $_POST['user'];
+		$author = $_POST['author'];
+    	$title = $_POST['title'];
+    	$chapo = $_POST['chapo'];
+    	$content = $_POST['content'];
+    	//$image = $_POST['image'];
+    	$change_date = $_POST['change_date'];
+
+    	supprimer($id, $user_id, $author, $title, $chapo, $content, $change_date);
+    	include(dirname(__FILE__).'/../vue/supprimer.php');
+
+	}
+	else
+	{
+		include(dirname(__FILE__).'/../vue/conexion.php');
+	}
+}
+?>
